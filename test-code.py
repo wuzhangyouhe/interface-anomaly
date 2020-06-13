b9a11aee6afd1d603ba22bb16413f0dbf63bba34
Tao Liu <taol@twitter.com>
2020-06-13 17:04:06 +0800

    kmeans

--- test-code.py
+++ test-code.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env ipython
 # Dependencies
 
 import pandas as pd
@@ -15,16 +16,16 @@ test_data = pd.read_csv("if_down_test.csv", skiprows=1)
 print(train_data.head())
 test_data.head()
 x= np.array(train_data)
+y = np.array(test_data)
 
 kmeans = KMeans(n_clusters=2) # You want cluster the passenger records into 2: Survived or Not survived
 kmeans.fit(x)
 y_kmeans = kmeans.predict(x)
-# plt.scatter(train_data[0], train_data[1])
-# plt.xlabel('Timestamp')
-# plt.ylabel('Value')
-# plt.show()
-plt.scatter(x[:, 0], x[:, 1], c=y_kmeans, s=50, cmap='viridis')
-
+plt.scatter(x[:,0], x[:,1], c=y_kmeans, cmap='viridis')
 centers = kmeans.cluster_centers_
 plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
+plt.xlabel('Timestamp')
+plt.ylabel('Value')
+plt.show()
+
 
